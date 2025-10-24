2e3e972e56ad07fb2098621d0beb8b381bab5627
Alexander <alex.m.vlasov@gmail.com>
2025-10-24 22:18:05 +0300

    temporary skip serialization

--- circuit_defs/prover_examples/src/unrolled.rs
+++ circuit_defs/prover_examples/src/unrolled.rs
@@ -1550,15 +1550,17 @@ pub fn prove_unrolled_execution_with_replayer<
 
         for (idx, chunk) in witness_chunks.into_iter().enumerate() {
             if should_dump_witness {
-                println!(
-                    "Will serialize witness for family {} circuit {}",
-                    family_idx, idx
-                );
-                bincode_serialize_to_file(
-                    &chunk.realloc_to_global(),
-                    &format!("family_{}_circuit_{}_oracle_witness.bin", family_idx, idx),
-                );
-                println!("Serialization is done");
+                if family_idx == 4 {
+                    println!(
+                        "Will serialize witness for family {} circuit {}",
+                        family_idx, idx
+                    );
+                    bincode_serialize_to_file(
+                        &chunk.realloc_to_global(),
+                        &format!("family_{}_circuit_{}_oracle_witness.bin", family_idx, idx),
+                    );
+                    println!("Serialization is done");
+                }
             }
 
             let oracle = NonMemoryCircuitOracle {
