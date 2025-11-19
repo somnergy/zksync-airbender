12c1e21233e7431947d1474d953d9251ed944a5a
Alexander <alex.m.vlasov@gmail.com>
2025-11-19 22:20:29 +0200

    remove features

--- tools/cli/src/vk.rs
+++ tools/cli/src/vk.rs
@@ -3,36 +3,38 @@ use execution_utils::Machine;
 use sha3::{Digest, Keccak256};
 
 pub fn generate_vk(bin_path: &String, machine: &Option<Machine>, output: &Option<String>) {
-    let binary = std::fs::read(bin_path).expect("Failed to read binary file");
+    todo!();
 
-    let mut hasher = Keccak256::new();
-    hasher.update(&binary);
-    let hash = hasher.finalize();
-    let bytecode_hash_hex = format!("{:x}", hash);
-    let params = execution_utils::generate_params_for_binary(
-        &binary,
-        machine.clone().unwrap_or(Machine::Standard),
-    );
+    // let binary = std::fs::read(bin_path).expect("Failed to read binary file");
 
-    let params_hex = params
-        .iter()
-        .map(|p| format!("{:08x}", p))
-        .collect::<Vec<_>>()
-        .join("");
+    // let mut hasher = Keccak256::new();
+    // hasher.update(&binary);
+    // let hash = hasher.finalize();
+    // let bytecode_hash_hex = format!("{:x}", hash);
+    // let params = execution_utils::generate_params_for_binary(
+    //     &binary,
+    //     machine.clone().unwrap_or(Machine::Standard),
+    // );
 
-    let vk = VerificationKey {
-        machine_type: machine.clone().unwrap_or(Machine::Standard),
-        bytecode_hash_hex,
-        params,
-        params_hex,
-    };
+    // let params_hex = params
+    //     .iter()
+    //     .map(|p| format!("{:08x}", p))
+    //     .collect::<Vec<_>>()
+    //     .join("");
 
-    println!("Verification key generated: {:?}", vk);
+    // let vk = VerificationKey {
+    //     machine_type: machine.clone().unwrap_or(Machine::Standard),
+    //     bytecode_hash_hex,
+    //     params,
+    //     params_hex,
+    // };
 
-    if let Some(output) = output {
-        let json = serde_json::to_string_pretty(&vk)
-            .expect("Failed to serialize verification key to JSON");
-        std::fs::write(output, json).expect("Failed to write verification key to output file");
-        println!("Verification key written to {}", output);
-    }
+    // println!("Verification key generated: {:?}", vk);
+
+    // if let Some(output) = output {
+    //     let json = serde_json::to_string_pretty(&vk)
+    //         .expect("Failed to serialize verification key to JSON");
+    //     std::fs::write(output, json).expect("Failed to write verification key to output file");
+    //     println!("Verification key written to {}", output);
+    // }
 }
